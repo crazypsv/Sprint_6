dd3d3e738ad03c74c3505180b13bdf8bfdfd03e5
Stas Puzachev <stanislavpuzachev@yandex.ru>
2025-07-16 20:35:21 +0300

    Имправил путь

--- locators/order_locators.py
+++ locators/order_locators.py
@@ -4,16 +4,16 @@ from selenium.webdriver.common.by import By
 class OrderLocators:
 
     # Кнопка "Заказать"
-    ORDER_BUTTON_HEADER = [By.XPATH, "//button[text()='Заказать']"]
-    ORDER_CENTER_BUTTON = [By.XPATH, '(//button[text()="Заказать"])[2]']
+    ORDER_BUTTON_HEADER = [By.XPATH, ".//div[starts-with(@class, 'Header')]/button[text()='Заказать']"]
+    ORDER_CENTER_BUTTON = [By.XPATH, ".//div[starts-with(@class, 'Home')]/button[text()='Заказать']"]
 
     # Данные пользователя
-    NAME = [By.XPATH, "//input[@placeholder='* Имя']"]
-    LAST_NAME = [By.XPATH, "//input[@placeholder='* Фамилия']"]
-    ADDRESS = [By.XPATH, "//input[@placeholder='* Адрес: куда привезти заказ']"]
-    METRO = [By.XPATH, "//input[@placeholder='* Станция метро']"]
+    NAME = [By.XPATH, ".//input[contains(@placeholder,'Имя')]"]
+    LAST_NAME = [By.XPATH, ".//input[contains(@placeholder,'Фамилия')]"]
+    ADDRESS = [By.XPATH, ".//input[contains(@placeholder,'Адрес')]"]
+    METRO = [By.XPATH, ".//input[contains(@placeholder,'метро')]"]
     LIST_STATION = [By.XPATH, "//li[@data-index='0']"]
-    NUMBER = [By.XPATH, "//input[@placeholder='* Телефон: на него позвонит курьер']"]
+    NUMBER = [By.XPATH, ".//input[contains(@placeholder,'Телефон')]"]
     NEXT_BUTTON = [By.XPATH, ".//button[text()='Далее']"]
 
     # Окно Про аренду
@@ -22,11 +22,11 @@ class OrderLocators:
     SELECT_RENT_TIME = [By.XPATH, '//div[text()="{}"]']
     BLACK_COLOR_CHECKBOX = [By.XPATH, '//label[@for="black"]']
     GREY_COLOR_CHECKBOX = [By.XPATH, '//label[@for="grey"]']
-    COMMENT = [By.XPATH, "//input[@placeholder='Комментарий для курьера']"]
-    ORDER_BUTTON = [By.XPATH, '//div[@class="Order_Buttons__1xGrp"]/button[text()="Заказать"]']
+    COMMENT = [By.XPATH, ".//input[contains(@placeholder,'Комментарий для курьера')]"]
+    ORDER_BUTTON = [By.XPATH, "(.//button[text()='Заказать'])[2]"]
 
     # Кнопка Да в сплывающем окне заказа подтверждения
     YES_BUTTON = [By.XPATH, ".//button[text()='Да']"]
 
     # Текс окна "Заказ оформлен"
-    ORDER_COMPLETED = [By.XPATH, '//div[contains(text(), "Заказ оформлен")]']
+    ORDER_COMPLETED = [By.XPATH, "//div[contains(@class, 'Order_ModalHeader')]"]
